6f00929ef2380a9ca698c89e9919440f6d20ab18
andyburnett <andy.burnett@knowinnovation.com>
2025-11-03 21:55:23 -0500

    Comment out ki-address.js import
    Comment out the import statement for ki-address.js.

--- ki-components.js
+++ ki-components.js
@@ -1,4 +1,4 @@
 // ki-components.js (The Bundle)
-import './components/ki-address.js'; 
+// import './components/ki-address.js'; 
 import './components/ki-guidelines.js'; 
 import './components/ki-badge.js';
